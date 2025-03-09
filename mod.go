bf4572ccf8d76a08d0f4dd8bc06ca92aa5487c54
Arief Darmawan <ariefda@hotmail.com>
2025-03-09 11:02:31 +0700

    add RegisterIntegration

--- mod.go
+++ mod.go
@@ -1,7 +1,13 @@
 package rayiapp
 
 import (
+	"fmt"
+
 	"git.kanosolution.net/kano/kaos"
+)
+
+var (
+	mapIntegrations = map[string][]string{}
 )
 
 func PostMWIntegration(ctx *kaos.Context, payload interface{}) (bool, error) {
@@ -14,10 +20,35 @@ func PostMWIntegration(ctx *kaos.Context, payload interface{}) (bool, error) {
 		return true, nil
 	}
 	go func() {
-		fnRes := ctx.Data().Get("FnResult", nil)
-		evi.Publish(apiPath, fnRes, nil, &kaos.PublishOpts{
-			Headers: ctx.Data().Data(),
-		})
+		subjects, has := mapIntegrations[apiPath]
+		if has {
+			for _, subject := range subjects {
+				fnRes := ctx.Data().Get("FnResult", nil)
+				go evi.Publish(
+					subject,
+					fnRes, nil, &kaos.PublishOpts{
+						Headers: ctx.Data().Data(),
+					})
+			}
+		}
 	}()
 	return true, nil
+}
+
+func RegisterIntegration(apiPath string, subject string) error {
+	subjects, has := mapIntegrations[apiPath]
+	if !has {
+		subjects = []string{}
+	}
+
+	for _, exSubject := range subjects {
+		if exSubject == subject {
+			return fmt.Errorf("integration already exist, path: %s, integration: %s", apiPath, subject)
+		}
+	}
+
+	subjects = append(subjects, subject)
+	mapIntegrations[apiPath] = subjects
+
+	return nil
 }
